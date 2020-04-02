716ffd403c875ce33510a829e47d62f1bf75f5f8
atpluem <atpluem@gmail.com>
2020-04-02 15:05:16 +0700

    initial commit

---
+++ server.js
@@ -0,0 +1,7 @@
+// Install express server
+const express = require('express');
+const app = express();
+// Serve only the static files from the dist directory
+app.use(express.static(__dirname + '/dist'));
+// Start the app by listening on the default Herokuport
+app.listen(process.env.PORT || 8080);
