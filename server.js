976f71e9dc5186458e0e2d1f104e6ab87ea4f0f2
atpluem <atpluem@gmail.com>
2020-04-17 18:32:04 +0700

    commit

--- server.js
+++ server.js
@@ -1,15 +1,13 @@
 //Install express server
 const express = require('express');
 const path = require('path');
- 
 const app = express();
  
-// Serve only the static files form the angularapp directory
-app.use(express.static(__dirname + '/angularapp'));
- 
+app.use(express.static(__dirname + '/dist'));
+app.listen(process.env.PORT || 8080);
+
 app.get('/*', function(req,res) {
-    res.sendFile(path.join(__dirname+'/angularapp/index.html'));
+    res.sendFile(path.join(__dirname+'/dist/index.html'));
 });
  
-// Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+console.log('Console listening');
