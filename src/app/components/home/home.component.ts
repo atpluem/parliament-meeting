537b325be13f4abc6a5130269466dafb4e2ac8bd
atpluem <atpluem@gmail.com>
2020-05-04 09:06:34 +0700

    Update home by ice

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
 import { LoginService } from '../../services/login.service';
+import { element } from 'protractor';
 import * as $ from 'jquery';
+
 
 @Component({
   selector: 'app-home',
@@ -31,6 +33,29 @@ export class HomeComponent implements OnInit {
   logout() {
     this.loginService.deleteToken();
     window.location.href = window.location.href;
+  }
+
+  /* function for smooth scrolling to another section */
+  SmoothTo($element) : void {
+    $element.scrollIntoView({behavior: "smooth"});
+  }
+
+  /* function for fix navigation bar in top */
+  @HostListener('window:scroll', ['$event'])
+  onWindowScroll(e) {
+    if(window.pageYOffset > 20){
+      let element = document.getElementById('navbar');
+      element.classList.add('sticky');
+    }
+    else {
+      let element = document.getElementById('navbar');
+      element.classList.remove('sticky');
+    }
+
+    /* part of parallax scorlling effect */
+    let parallax = document.getElementById('parallax');
+    let offset = window.pageYOffset;
+    parallax.style.backgroundPositionY = offset * 0.09 + "px";
   }
 
   ngOnInit(): void {
