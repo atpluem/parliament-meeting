34323169ca7a46e8a272e71d74c70a4cf8d0d84b
atpluem <atpluem@gmail.com>
2020-05-16 23:45:18 +0700

    update view

--- src/app/components/view/conference-view/conference-view.component.ts
+++ src/app/components/view/conference-view/conference-view.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ViewService } from 'src/app/services/view.service';
 
 @Component({
   selector: 'app-conference-view',
@@ -6,11 +7,40 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./conference-view.component.css']
 })
 export class ConferenceViewComponent implements OnInit {
+  conference: any;
 
-  constructor() { }
+  constructor(private view: ViewService) { }
+
+  getConference() {
+    this.view.getConference()
+    .subscribe(data => {
+      this.conference = data;
+      console.log("<<<"+this.conference+">>>");
+    });
+  }
 
   ngOnInit(): void {
+    this.getConference();
+
+    $("#showModalConference").click(function() {
+      $(".conference").addClass("is-active");  
+    });
+
+    $("#showModalArgument").click(function() {
+      $(".argument").addClass("is-active");  
+    });
+
+    $("#showModalAttendees").click(function() {
+      $(".attendees").addClass("is-active");  
+    });
+
+    $("#showModalCost").click(function() {
+      $(".cost").addClass("is-active");  
+    });
     
+    $(".delete").click(function() {
+       $(".modal").removeClass("is-active");
+    });
   }
 
 }
